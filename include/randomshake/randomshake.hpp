5e57f0e019432642918e1ce27a8b9e7788d3a419
Anjan Roy <hello@itzmeanjan.in>
2024-11-05 16:15:25 +0400

    Implement RandomShake cryptographically secure pseudo random number generator
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/randomshake/randomshake.hpp
@@ -0,0 +1,173 @@
+#pragma once
+#include "sha3/shake256.hpp"
+#include <array>
+#include <bit>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <type_traits>
+
+namespace randomshake {
+
+// Compile-time check to ensure that bit-security level is valid.
+consteval auto
+check_bit_security_level(size_t bit_security_level)
+{
+  return (bit_security_level == 128u) || (bit_security_level == 192u) || (bit_security_level == 256u);
+}
+
+// Compile-time check to ensure that endianness is little, as correctness of program depends on it.
+consteval auto
+check_endianness()
+{
+  return std::endian::native == std::endian::little;
+}
+
+/**
+  After squeezing every these many bytes from underlying SHAKE256 Xof, we zeroize first `n` bytes of
+  Keccak permutation state and re-apply permutation.
+ */
+static constexpr size_t RANDOMSHAKE_RATCHET_PERIOD_BYTE_LEN = shake256::RATE;
+
+/**
+  Ensures that value is materialized (and not optimized away), but doesn't clobber memory, like google-benchmark does.
+
+  Taken from https://theunixzoo.co.uk/blog/2021-10-14-preventing-optimisations.html.
+ */
+template<typename Tp>
+forceinline void
+DoNotOptimize(Tp& value)
+{
+  asm volatile("" : "+r,m"(value) : :);
+}
+
+/**
+  RandomShake - SHAKE256 -backed *C*ryptographically *S*ecure *P*seudo *R*andom *N*umber *G*enerator.
+
+  Allowing both (a) `std::random_device` sampled seed, (b) User provided seed -based initialization of CSPRNG.
+  After every `RANDOMSHAKE_RATCHET_PERIOD_BYTE_LEN` -many bytes are squeezed from SHAKE256 Xof instance, we perform
+  ratcheting i.e. zeroing out of first `ratchet_byte_len` -many bytes of Keccak permutation state and re-applying
+  permutation.
+
+  Design of this CSPRNG collects inspiration from https://seth.rocks/articles/cpprandom.
+ */
+template<size_t bit_security_level, typename UIntType = uint8_t>
+  requires(check_bit_security_level(bit_security_level) && std::is_unsigned_v<UIntType> && check_endianness())
+struct randomshake_t
+{
+private:
+  shake256::shake256_t state{};
+  std::array<uint8_t, RANDOMSHAKE_RATCHET_PERIOD_BYTE_LEN> buffer{};
+  size_t buffer_offset = 0;
+
+  // These many bytes are zeroed from the beginning of the Keccak permutation state during ratchet operation.
+  const size_t ratchet_byte_len = std::min(shake256::RATE, bit_security_level) / std::numeric_limits<uint8_t>::digits;
+
+public:
+  using result_type = UIntType;
+
+  static constexpr auto seed_byte_len = bit_security_level / std::numeric_limits<uint8_t>::digits;
+  static constexpr auto min = std::numeric_limits<result_type>::min;
+  static constexpr auto max = std::numeric_limits<result_type>::max;
+
+  // Samples `seed_byte_len` -many bytes from std::random_device and initializes SHAKE256 Xof - making it ready for use.
+  forceinline randomshake_t()
+  {
+    std::array<uint8_t, seed_byte_len> seed{};
+    auto seed_span = std::span(seed);
+
+    std::random_device rd{};
+    const auto entropy = rd.entropy();
+    if (entropy == 0.) {
+      std::cout << "[RANDOMSHAKE WARNING] Non-deterministic seed generator has zero entropy ! "
+                   "Read https://en.cppreference.com/w/cpp/numeric/random/random_device/entropy for more insight.\n";
+    }
+
+    constexpr size_t step_by = sizeof(std::random_device::result_type);
+    size_t seed_offset = 0;
+    while (seed_offset < seed_span.size()) {
+      const auto v = rd();
+
+      static_assert(seed_byte_len % step_by == 0,
+                    "Seed byte length must be a multiple of `step_by`, for following memcpy to work correctly !");
+      std::memcpy(seed_span.subspan(seed_offset, step_by).data(), reinterpret_cast<const uint8_t*>(&v), step_by);
+
+      seed_offset += step_by;
+    }
+
+    // Using bit-security level as domain separator, so that using same seed doesn't produce same output stream.
+    constexpr uint16_t domain_separator = static_cast<uint16_t>(bit_security_level);
+    auto ds_span = std::span(reinterpret_cast<const uint8_t*>(&domain_separator), sizeof(domain_separator));
+
+    state.absorb(seed_span);
+    state.absorb(ds_span);
+    state.finalize();
+    state.squeeze(buffer);
+  }
+
+  // Expects user to supply us with `seed_byte_len` -bytes seed, which is used for initializing underlying SHAKE256 Xof.
+  forceinline explicit constexpr randomshake_t(std::span<const uint8_t, seed_byte_len> seed)
+  {
+    // Using bit-security level as domain separator, so that using same seed doesn't produce same output stream.
+    constexpr uint16_t domain_separator = static_cast<uint16_t>(bit_security_level);
+    auto ds_span = std::span(reinterpret_cast<const uint8_t*>(&domain_separator), sizeof(domain_separator));
+
+    state.absorb(seed);
+    state.absorb(ds_span);
+    state.finalize();
+    state.squeeze(buffer);
+  }
+
+  // Delete copy and move constructors - as this CSPRNG instance is neither copyable nor movable.
+  randomshake_t(const randomshake_t&) = delete;
+  randomshake_t(randomshake_t&&) = delete;
+  randomshake_t& operator=(const randomshake_t&) = delete;
+  randomshake_t& operator=(randomshake_t&&) = delete;
+
+  // Zeroize internal state when destroying an instance of CSPRNG.
+  ~randomshake_t()
+  {
+    DoNotOptimize(state);
+    state.reset();
+    DoNotOptimize(state);
+
+    DoNotOptimize(buffer);
+    buffer.fill(0);
+    DoNotOptimize(buffer);
+
+    DoNotOptimize(buffer_offset);
+    buffer_offset = 0;
+    DoNotOptimize(buffer_offset);
+  }
+
+  // Squeezes random value of type `result_type`.
+  result_type operator()()
+  {
+    constexpr size_t required_num_bytes = sizeof(result_type);
+    const size_t readble_num_bytes = buffer.size() - buffer_offset;
+
+    static_assert(
+      buffer.size() % required_num_bytes == 0,
+      "Buffer size nust be a multiple of `required_num_bytes`, for following ratchet->squeeze to work correctly !");
+
+    // When the buffer is exhausted, it's time to ratchet and fill the buffer with new ready-to-use random bytes.
+    if (readble_num_bytes == 0) {
+      state.ratchet(ratchet_byte_len);
+      state.squeeze(buffer);
+      buffer_offset = 0;
+    }
+
+    result_type result{};
+
+    auto src_ptr = reinterpret_cast<const uint8_t*>(buffer.data()) + buffer_offset;
+    auto dst_ptr = reinterpret_cast<uint8_t*>(&result);
+
+    std::memcpy(dst_ptr, src_ptr, required_num_bytes);
+    buffer_offset += required_num_bytes;
+
+    return result;
+  }
+};
+
+}
