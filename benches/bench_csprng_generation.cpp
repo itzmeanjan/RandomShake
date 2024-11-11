7a1ea5b913e6339810bb9347abd6713a36c468c2
Anjan Roy <hello@itzmeanjan.in>
2024-11-11 11:19:47 +0400

    Add benchmark function for byte sequence squeezing
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/bench_csprng_generation.cpp
+++ benches/bench_csprng_generation.cpp
@@ -2,6 +2,7 @@
 #include "randomshake/randomshake.hpp"
 #include <array>
 #include <benchmark/benchmark.h>
+#include <vector>
 
 template<size_t bit_security_level, typename result_type>
 static void
@@ -27,6 +28,32 @@ bench_csprng_output_generation(benchmark::State& state)
   state.SetBytesProcessed(state.iterations() * sizeof(result_type));
 }
 
+template<size_t bit_security_level>
+static void
+bench_csprng_byte_sequence_squeezing(benchmark::State& state)
+{
+  std::array<uint8_t, randomshake::randomshake_t<bit_security_level>::seed_byte_len> seed{};
+  seed.fill(0xde);
+
+  randomshake::randomshake_t<bit_security_level> csprng(seed);
+
+  constexpr size_t RANDOM_OUTPUT_BYTE_LEN = 1'024 * 1'024; // 1 MB
+  std::vector<uint8_t> rand_byte_seq(RANDOM_OUTPUT_BYTE_LEN, 0);
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(&csprng);
+    benchmark::DoNotOptimize(rand_byte_seq);
+
+    csprng.generate(rand_byte_seq);
+
+    benchmark::DoNotOptimize(&csprng);
+    benchmark::DoNotOptimize(rand_byte_seq);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetBytesProcessed(state.iterations() * rand_byte_seq.size());
+}
+
 BENCHMARK(bench_csprng_output_generation<256, uint8_t>)
   ->Name("csprng/256b/generate_u8")
   ->ComputeStatistics("min", compute_min)
@@ -41,5 +68,18 @@ BENCHMARK(bench_csprng_output_generation<256, uint32_t>)
   ->ComputeStatistics("max", compute_max);
 BENCHMARK(bench_csprng_output_generation<256, uint64_t>)
   ->Name("csprng/256b/generate_u64")
+  ->ComputeStatistics("min", compute_min)
+  ->ComputeStatistics("max", compute_max);
+
+BENCHMARK(bench_csprng_byte_sequence_squeezing<128>)
+  ->Name("csprng/128b/generate_byte_seq")
+  ->ComputeStatistics("min", compute_min)
+  ->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_csprng_byte_sequence_squeezing<192>)
+  ->Name("csprng/192b/generate_byte_seq")
+  ->ComputeStatistics("min", compute_min)
+  ->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_csprng_byte_sequence_squeezing<256>)
+  ->Name("csprng/256b/generate_byte_seq")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
