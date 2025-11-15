993b312c4b0def570179ad1f32fc7db1dfb26df3
Anjan Roy <hello@itzmeanjan.in>
2025-11-15 19:35:01 +0530

    Fix issue in same seed vs. diff seed test in case of deterministic CSPRNG
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- tests/test_deterministic_csprng.cpp
+++ tests/test_deterministic_csprng.cpp
@@ -14,16 +14,14 @@ TEST(RandomShake, Deterministic_CSPRNG_Using_Same_Seed_Produces_Eq_Output)
   std::array<uint8_t, randomshake::randomshake_t<>::seed_byte_len> seed{};
   seed.fill(0xde);
 
-  auto rand_gen = [&]() {
-    randomshake::randomshake_t<> csprng(seed);
-    return csprng();
-  };
-
   std::vector<uint8_t> rand_bytes_a(GENERATED_RANDOM_BYTE_LEN, 0x00);
   std::vector<uint8_t> rand_bytes_b(GENERATED_RANDOM_BYTE_LEN, 0xff);
 
-  std::ranges::generate(rand_bytes_a, rand_gen);
-  std::ranges::generate(rand_bytes_b, rand_gen);
+  randomshake::randomshake_t<> csprng_a(seed);
+  std::ranges::generate(rand_bytes_a, [&]() { return csprng_a(); });
+
+  randomshake::randomshake_t<> csprng_b(seed);
+  std::ranges::generate(rand_bytes_b, [&]() { return csprng_b(); });
 
   EXPECT_EQ(rand_bytes_a, rand_bytes_b);
 }
@@ -33,18 +31,17 @@ TEST(RandomShake, Deterministic_CSPRNG_Using_Diff_Seed_Produces_Ne_Output)
   std::array<uint8_t, randomshake::randomshake_t<>::seed_byte_len> seed{};
   seed.fill(0xde);
 
-  auto rand_gen = [&]() {
-    randomshake::randomshake_t<> csprng(seed);
-    return csprng();
-  };
-
   std::vector<uint8_t> rand_bytes_a(GENERATED_RANDOM_BYTE_LEN, 0x00);
   std::vector<uint8_t> rand_bytes_b(GENERATED_RANDOM_BYTE_LEN, 0x00);
 
-  std::ranges::generate(rand_bytes_a, rand_gen);
+  randomshake::randomshake_t<> csprng_a(seed);
+  std::ranges::generate(rand_bytes_a, [&]() { return csprng_a(); });
 
+  seed.fill(0xde);
   randomshake_test_utils::do_bitflip(seed[0], 3);
-  std::ranges::generate(rand_bytes_b, rand_gen);
+
+  randomshake::randomshake_t<> csprng_b(seed);
+  std::ranges::generate(rand_bytes_b, [&]() { return csprng_b(); });
 
   EXPECT_NE(rand_bytes_a, rand_bytes_b);
 }
